126683e38cc142f3d1794c741153d204262aac85
vitor71pt <vitor71rodrigues@gmail.com>
2023-03-15 22:23:53 +0000

    Generate new version

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it! New Version" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
